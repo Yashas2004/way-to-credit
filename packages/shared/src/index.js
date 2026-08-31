d40e9c376d9dd4971f18270bb609742ed9e2091a
Yashas2004 <yashas.r.2004.07@gmail.com>
2026-08-31 22:12:46 +0530

    chore: scaffold monorepo foundation

--- packages/shared/src/index.js
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-export const HealthResponseSchema = z.object({
-  status: z.literal("ok"),
-  uptime: z.number(),
-  version: z.string(),
-});
-//# sourceMappingURL=index.js.map
